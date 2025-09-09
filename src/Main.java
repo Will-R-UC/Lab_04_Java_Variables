f8e79588497253f68396953373104387dba02694
Will-R-UC <rahtzwd@mail.uc.edu>
2025-09-09 13:15:23 -0400

    Adds integer and double math tests

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //int operands
+        int intOperandA = 5;
+        int intOperandB = 4;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print("\n");
+        System.out.println(">>> Integer Math <<<");
+
+        //int sum
+        int intSum = 100;
+        intSum = intOperandA + intOperandB;
+        System.out.println("For integers, " + intOperandA + " plus " + intOperandB + " equals " + intSum);
+
+        //int product
+        int intProduct = 200;
+        intProduct = intOperandA * intOperandB;
+        System.out.println("For integers, " + intOperandA + " times " + intOperandB + " equals " + intProduct);
+
+        //int difference
+        int intDifference = 300;
+        intDifference = intOperandA - intOperandB;
+        System.out.println("For integers, " + intOperandA + " minus " + intOperandB + " equals " + intDifference);
+
+        //int quotient
+        int intQuotient = 400;
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("For integers, " + intOperandA + " divided by " + intOperandB + " equals " + intQuotient);
+
+        int intModulo = 500;
+        intModulo = intOperandA % intOperandB;
+        System.out.println("For integers, " + intOperandA + " mod " + intOperandB + " equals " + intModulo);
+
+        //double operands
+        double doubleOperandA = 2.5;
+        double doubleOperandB = 3.5;
+
+        System.out.print("\n");
+        System.out.println(">>> Double Math <<<");
+
+        //double sum
+        double doubleSum = 100.0;
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("For doubles, " + doubleOperandA + " plus " + doubleOperandB + " equals " + doubleSum);
+
+        //double product
+        double doubleProduct = 200.0;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("For doubles, " + doubleOperandA + " times " + doubleOperandB + " equals " + doubleProduct);
+
+        //double difference
+        double doubleDifference = 300.0;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("For doubles, " + doubleOperandA + " minus " + doubleOperandB + " equals " + doubleDifference);
+
+        //double quotient
+        double doubleQuotient = 400.0;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("For doubles, " + doubleOperandA + " divided by " + doubleOperandB + " equals " + doubleQuotient);
+
     }
 }
